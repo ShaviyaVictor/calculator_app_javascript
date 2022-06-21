db9872b24470075db1337b7e9bc1381b39fbc1e9
Victor Shaviya <vs610642@digicelgroup.com>
2022-06-21 13:21:29 -0500

    Create a class and a constructor to hold our input values

--- assets/logic.js
+++ assets/logic.js
@@ -1,3 +1,12 @@
+// create a calculator class to hold our inputs
+class Calclator {
+  // create the class constructor
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+}
+
 // select all the buttons and operands in the html file
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
