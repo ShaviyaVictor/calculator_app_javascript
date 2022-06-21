76a5c3a9b01b6cc18afba553e89bc90c288bc3f3
Victor Shaviya <vs610642@digicelgroup.com>
2022-06-21 14:11:45 -0500

    Create the equal logic as well as the compute logic function for the operations

--- assets/logic.js
+++ assets/logic.js
@@ -38,9 +38,32 @@ class Calculator {
   }
 
   compute() {
-
+    let computation 
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current))
+      return
+    switch (this.operation) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      case '*':
+        computation = prev * current
+        break
+      case '÷':
+        computation = prev / current
+        break
+      default:
+        return
+    }
+    this.currentOperand =computation
+    this.operation = undefined
+    this.previousOperand = ''
   }
-  
+
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand
     this.previousOperandTextElement.innerText = this.previousOperand
@@ -72,4 +95,9 @@ operationButtons.forEach(button => {
     calculator.chooseOperation(button.innerText)
     calculator.updateDisplay()
   })
+})
+
+equalButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
 })
