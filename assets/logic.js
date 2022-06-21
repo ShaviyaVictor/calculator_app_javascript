82f89d8432fc00d65c13a5e59c68215dc11772b7
Victor Shaviya <vs610642@digicelgroup.com>
2022-06-21 14:39:34 -0500

    Work outthe decimal digits value inconsistence

--- assets/logic.js
+++ assets/logic.js
@@ -64,9 +64,36 @@ class Calculator {
     this.previousOperand = ''
   }
 
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    }
+    else {
+      integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0
+      })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    }
+    else {
+      return integerDisplay
+    }
+    // const floatNumber = parseFloat(number)
+    // if (isNaN(floatNumber))
+    //   return ''
+    // return floatNumber.toLocaleString('en')
+  }
+
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand
-    this.previousOperandTextElement.innerText = this.previousOperand
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
+    }
   }
 }
 
