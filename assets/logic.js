c9e01421b453152d0ab56c59e0e18630f5a5080c
Victor Shaviya <vs610642@digicelgroup.com>
2022-06-21 13:15:24 -0500

    Create consts to select the elements from the html document

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,9 @@
 // select all the buttons and operands in the html file
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+
+const equalButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
